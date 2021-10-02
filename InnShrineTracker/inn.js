3262c23795e57d0406275c74bccacbc99c79a50d
Lonnie Parker <lonnie@gmail.com>
2021-10-01 23:45:59 -0400

    HTML Table now generated dynamically.

--- InnShrineTracker/inn.js
+++ InnShrineTracker/inn.js
@@ -12,35 +12,69 @@ const select_info = [
     {name: "storeroom", value: "Storeroom"}
 ];
 
+const shrine_symbols = [
+                        "ouroboros",
+                        "skull",
+                        "helmet",
+                        "pentagram",
+                        "hourglass",
+                        "bull",
+                        "eye",
+                        "sword",
+                        "cross",
+                        "flame"
+                       ];
+
 const exclude_options = new Set();
 
 function initializeHandlers() {
-    $('.roomselect').change(function() {
-        exclude_options.clear();
-
-        var selects = document.getElementsByClassName('roomselect');
-
-        for (var i = 0; i < selects.length; i++) {
-            if (selects.item(i).value == "blank")
-                continue;
-
-            exclude_options.add(selects.item(i).value);
-            //console.log(selects.item(i).value);
-        }
-
-        var opts = document.getElementsByClassName('opt');
-        //console.log(opts.length);
-
-        for (var i = 0; i < opts.length; i++) {
-            opts.item(i).disabled = exclude_options.has(opts.item(i).value);
-        }
-    });
-
     $('#reset').on('click', function() {
         $('.roomselect').val("blank");
 
         //Only call the change for one roomselect and not all since we want to clear the disabled flag
         $('.roomselect').first().change();
+    });
+
+    $(document).ready(function() {
+        console.log("document is ready");
+        tableBody = $('#shrine_table');
+
+        for (var i = 0; i < shrine_symbols.length; i += 2) {
+            var markup = "<tr>";
+
+            for (var c = i, max = c + 2; c < max; c++) {
+                markup += "<td><img src=\"img/" + shrine_symbols[c] + ".png\" alt=\"" + shrine_symbols[c] + "\" width=100></img></td>";
+                markup += "<td>";
+                markup += "<select name =\"" + shrine_symbols[c] + "_room\" id=\"" + shrine_symbols[c] + "_room\" class=\"roomselect\"></select>";
+                markup += "</td>";
+            }
+
+            markup += "</tr>";
+            tableBody.append(markup);
+        }
+
+        initializeSelects();
+
+        $('.roomselect').change(function() {
+            exclude_options.clear();
+    
+            var selects = document.getElementsByClassName('roomselect');
+    
+            for (var i = 0; i < selects.length; i++) {
+                if (selects.item(i).value == "blank")
+                    continue;
+    
+                exclude_options.add(selects.item(i).value);
+                //console.log(selects.item(i).value);
+            }
+    
+            var opts = document.getElementsByClassName('opt');
+            //console.log(opts.length);
+    
+            for (var i = 0; i < opts.length; i++) {
+                opts.item(i).disabled = exclude_options.has(opts.item(i).value);
+            }
+        });
     });
 }
 
@@ -65,4 +99,3 @@ function initializeSelects() {
 }
 
 initializeHandlers();
-initializeSelects();
