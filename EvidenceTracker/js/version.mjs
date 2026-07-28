2e18515d3a9c1f04b4202c9e0f4766365fecf955
Lonnie Parker <greemeister@gmail.com>
2026-07-28 09:00:02 -0400

    Update version

--- EvidenceTracker/js/version.mjs
+++ EvidenceTracker/js/version.mjs
@@ -1,2 +1,2 @@
-export const last_updated = "07-21-2026"
-export const phasmophobia_server_version = "0.18.0.0"
+export const last_updated = "07-28-2026"
+export const phasmophobia_server_version = "0.18.0.1"
