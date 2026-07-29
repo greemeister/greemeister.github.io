46e46c14107c53c0548f8728f651dbac5275fa52
Lonnie Parker <greemeister@gmail.com>
2026-07-29 00:02:49 -0400

    2

--- EvidenceTracker/js/evidenceTracker.js
+++ EvidenceTracker/js/evidenceTracker.js
@@ -1,4 +1,4 @@
-import { last_updated, phasmophobia_server_version } from './version.mjs';
+import { server_updated, phasmophobia_server_version } from './version.mjs';
 
 // Ghost Info Defs
 const ghostInfos = [
@@ -38,6 +38,7 @@ const maxNumOfEvidences = ghostInfos[0].evidences.length;
 const ecn_enabled = "enabled";
 const ecn_excluded = "excluded";
 const ecn_tagged = "tagged";
+const last_updated = "07-28-2026"
 
 var excludeEvidence = false;
 var excludeMode = false;
