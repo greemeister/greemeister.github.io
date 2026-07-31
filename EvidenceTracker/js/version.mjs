e9beff5f161f82d73db17ca6dca6e6b3a852489a
Lonnie Parker <greemeister@gmail.com>
2026-07-31 10:29:57 -0400

    Update version

--- EvidenceTracker/js/version.mjs
+++ EvidenceTracker/js/version.mjs
@@ -1,2 +1,3 @@
 export const server_updated = "07-28-2026"
 export const phasmophobia_server_version = "0.18.0.1"
+export const patch_notes_url = "https://steamstore-a.akamaihd.net/news/externalpost/steam_community_announcements/1839041357038727"
